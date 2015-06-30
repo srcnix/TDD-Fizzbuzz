fdf81e01816915b1e2623185bef191f8b5f8a796
Steve Clarke <srcnix@gmail.com>
2015-06-30 05:17:44 +0100

    Statement now in numerical order

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,10 +2,10 @@ class Fixnum
   def fizzbuzz
     if self % 3 == 0 && self % 5 == 0
       return 'fizz buzz'
-    elsif self % 5 == 0
-      return 'buzz'
     elsif self % 3 == 0
       return 'fizz'
+    elsif self % 5 == 0
+      return 'buzz'
     end
 
     self
